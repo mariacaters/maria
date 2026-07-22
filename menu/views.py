5208055f0dfc8338bdd49dd8461deef86dfa052c
mariacaters <mariacaters5@gmail.com>
2026-07-22 18:59:55 +0530

    Update views.py

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
-
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.template.loader import render_to_string
